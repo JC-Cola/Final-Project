47ee298240d73031c6034d4fbc194062a846cc60
JC-Cola <jc.cola2020@gmail.com>
2025-06-11 14:29:36 -0400

    asdf Co-authored-by: dsg6246 <dsg6246@users.noreply.github.com>

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (!timeSelect.value) {
       alert("Please select a time before starting the test.");
     } else {
-      window.location.href = "../Home/Test_page/index.html";
+      window.location.href = "../test.js";
     }
   });
 });
